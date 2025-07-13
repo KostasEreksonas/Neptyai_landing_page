4d98fde4a8bfb3f570c2fb8b52730ab30d7a1a1a
KostasEreksonas <k.ereksonas@gmail.com>
2025-07-13 22:27:05 +0300

    v0.1

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {Homepage} from './components/homepage/homepage';
+import {Navigation} from './components/navigation/navigation';
+import {Products} from './components/products/products';
+import {Footer} from './components/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    Homepage,
+    Navigation,
+    Products,
+    Footer
+  ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
